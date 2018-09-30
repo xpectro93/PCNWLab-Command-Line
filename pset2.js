abf0191ec96ab9573f0eb5d3ef402701955c6570
Mohammed Mosayed <mohammedmosayed@gmail.com>
2018-09-30 18:47:28 -0400

    pset2

---
+++ pset2.js
@@ -0,0 +1,26 @@
+/******************
+PROBLEM 1:
+
+
+1. Download this repository from the homepage by clicking "Clone or download", then "Download ZIP". You will be using the files from the /stuff directory to acheieve the following.
+2. Once the repository has been downloaded, go ahead and unzip it.
+
+3. Achieve the following structure ONLY using command line:
+
+~/Desktop/pset2
+    /assets
+        /images
+            amazon.jpg
+            facebook.png
+            google.jpg
+            twitter.png
+        /memes
+            meme.jpg
+        /src
+            foo.js
+    app.js
+    hello.html
+
+4. Open `hello.html` using a code editor and change "SOME_CLASS" to "Full Stack: Nights & Weekends". Save it.
+
+******************/
