72cc2d506e0d1703450de19587f452c51e4eb32a
Mohammed Mosayed <mohammedmosayed@gmail.com>
2018-09-30 18:13:03 -0400

    Create pset1.js

---
+++ pset1.js
@@ -0,0 +1,41 @@
+/******************
+PROBLEM 1:
+
+Follow the following steps ONLY using your command line:
+
+1. Navigate to your Desktop directory.
+2. Create a directory called my_first_direc.
+3. Enter the my_first_direc directory.
+4. Create a file called first.js.
+5. Create a folder called assignments.
+6. Move first.js into assignments.
+7. Go into the assignments folder.
+8. Delete first.js.
+9. Go up to the my_first_direc directory.
+10. Delete the assignments folder.
+11. Go up to the Desktop directory.
+12. Delete the test folder.
+
+******************/
+
+
+
+/******************
+PROBLEM 2:
+
+Follow the following steps ONLY using your command line:
+
+1. Navigate to the inside of your Documents folder
+3. Create a file called foo.js
+4. Open the file with a text editor, (Atom/VSCode)
+5. Add the following text to the file: console.log('hello')
+6. Save and exit the file
+7. Open the file again with a text editor, (Atom/VSCode)
+8. Add the following in a new line: hellohellohello
+9. Exit without saving
+
+******************/
+
+
+
+
