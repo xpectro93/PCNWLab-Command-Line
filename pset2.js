454ec68e8986c55a977637f161991a1c02605f21
Mohammed Mosayed <mohammedmosayed@gmail.com>
2018-09-30 19:18:50 -0400

    Update pset2.js

--- pset2.js
+++ pset2.js
@@ -1,8 +1,9 @@
 /******************
 PROBLEM 1:
 
+In this problem, you will be downloading some files from online, then creating a custom project with git initialized. You will then one by one structure the files and make commits to git as needed.
 
-1. Download this repository from the homepage by clicking "Clone or download", then "Download ZIP". You will be using the files from the /stuff directory to acheieve the following.
+1. Download this repository from the homepage by clicking "Clone or download", then "Download ZIP". You will be using the files from the /stuff directory.
 2. Once the repository has been downloaded, go ahead and unzip it.
 
 3. Achieve the following structure ONLY using command line:
@@ -23,4 +24,12 @@ PROBLEM 1:
 
 4. Open `hello.html` using a code editor and change "SOME_CLASS" to "Full Stack: Nights & Weekends". Save it.
 
+5. You should have the following commits:
+- git initialized
+- added app.js
+- added hello.html
+- created assets directory
+- added all images & memes
+- added all sources
+- modified hello file
 ******************/
